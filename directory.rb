9ec8e26b346c66254d7f0fab5c3772ec731eb394
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-04 13:52:29 +0100

    Directory.rb updated to use methods

--- directory.rb
+++ directory.rb
@@ -14,14 +14,26 @@ students = [
   "Norman Bates",
 ]
 
-
-puts "The students of Villains Academy"
-puts "-------------"
-#instead of printing out each student we can use a loop to itirate over the array.
-students.each do |name|
-  puts name
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-puts "Overall, we have #{students.count} great students"
+#instead of printing out each student we can use a loop to itirate over the array.
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
 #using string interpolation we can refactor the code so it looks cleaner.
 #we can use the .count() method to get the number of student.
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#we need to call the methods with any relevent arguments to get the results.
+
+print_header
+print(students)
+print_footer(students)
