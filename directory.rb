28f27b724c32201fe38e648cc15714ca99d6a8ee
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-04 14:43:52 +0100

    Directory is now interactive

--- directory.rb
+++ directory.rb
@@ -1,21 +1,18 @@
-#We can use a hash to add extra infomation to each student, making it easier to call.
-
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "to finish, please hit return twice"
+  name = gets.chomp
+  students = []
+  while !name.empty?
+    students << {name: name, cohort: :april}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of my cohort at Makers"
   puts "-------------"
 end
 
@@ -33,7 +30,7 @@ def print_footer(names)
 end
 
 #we need to call the methods with any relevent arguments to get the results.
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
