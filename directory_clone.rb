87ea3695124e096d1163ce85b9f127823ee7535a
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-06 10:28:27 +0100

    included new method add_students

--- directory_clone.rb
+++ directory_clone.rb
@@ -1,60 +1,111 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "to finish, please hit return twice"
-  name = gets.delete!("\n")
-  puts "Please let me know what cohort they are on"
-  cohort = gets.delete!("\n")
-  if cohort.empty?
-    cohort = "Not suplied"
-  end
+  @name = gets.chomp
+  add_students
+end
 
-  students = []
-  while !name.empty?
-    students << {name: name, cohort: cohort.to_sym}
-    if students.length == 1
-      puts "Now we have #{students.count} student"
-    else
-      puts "Now we have #{students.count} students"
-    end
-    name = gets.delete!("\n")
-    if name.empty?
-      break
-    end
-
-    cohort = gets.delete!("\n")
-    if cohort.empty?
-      cohort = "Not suplied"
-    end
+def add_students
+  while !@name.empty?
+    @students << {name: @name, cohort: :april}
+    puts "Now we have #{@students.count} students"
+    @name = gets.chomp
   end
-  students
 end
 
 def print_header
-  puts "What cohort would you like to see?"
-  @cohort = gets.delete!("\n")
   puts "The students of my cohort at Makers"
   puts "-------------"
 end
 
 #instead of printing out each student we can use a loop to itirate over the array.
-def print(names)
-  names.map do |name|
-    puts "#{name[:name]} (cohort: #{name[:cohort]})" if name[:cohort] == @cohort.to_sym
+def print_students_list
+  @students.each do |name|
+    puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
 #using string interpolation we can refactor the code so it looks cleaner.
 #we can use the .count() method to get the number of student.
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 #we need to call the methods with any relevent arguments to get the results.
-students = input_students
-if students.length < 1
-  puts "You need to add some students"
-  students = input_students()
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save list to students.csv"
+  puts "4. Load list from students.csv"
+  puts "9. Exit"
 end
-print_header
-print(students)
-print_footer(students)
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      puts "Have a good day"
+      exit
+    else
+      puts "I dont know what you mean, please try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w") # open the file for writing. csv - comma seperated format
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename="students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    #name gets first part of the split array, cohort second (parallel assignment)
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "Loaded #{@students.count} students from #{filename}"
+end
+
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  if filename.nil?
+    load_students
+  elsif File.exists?(filename) #if it exists
+    load_students(filename)
+  else
+    puts "Sorry, #{filename} does not exist"
+    exit
+  end
+end
+
+try_load_students
+interactive_menu
